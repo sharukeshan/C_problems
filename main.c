adf4d01070c11ce58795586f66005046a86324d5
sharukeshan <93191464+sharukeshan@users.noreply.github.com>
2023-07-03 20:47:32 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ 
+int main() {
+    int num;
+    printf("Enter an integer\n");
+    scanf("%d", &num);
+     
+    printf("7 x %d = %d",num,(num << 3)-num);
+ 
+    return 0;
+}
+
